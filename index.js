f72dcf5cf5d28f8c4681dc6dbec4b8c0d88b513b
wvdh <wanda@vandenhoogen.eu>
2025-06-08 15:43:01 +0200

    updates

---
+++ index.js
@@ -0,0 +1,45 @@
+import { tweetsData } from './data.js'
+
+const tweetInput = document.getElementById('tweet-input')
+const tweetBtn = document.getElementById('tweet-btn')
+
+tweetBtn.addEventListener('click', function(){
+    console.log(tweetInput.value)
+})
+
+function getFeedHtml(){
+        let feedHtml = ``
+        tweetsData.forEach(function(tweet){
+        feedHtml += `
+        <div class="tweet">
+            <div class="tweet-inner">
+                <img src="${tweet.profilePic}" class="profile-pic">
+                <div>
+                    <p class="handle">${tweet.handle}</p>
+                    <p class="tweet-text">${tweet.tweetText}</p>
+                    <div class="tweet-details">
+                        <span class="tweet-detail">
+                            ${tweet.replies.length}
+                        </span>
+                        <span class="tweet-detail">
+                            ${tweet.likes}
+                        </span>
+                        <span class="tweet-detail">
+                            ${tweet.retweets}
+                        </span>
+                    </div>   
+                </div>            
+            </div>
+        </div>`
+    })
+    return feedHtml
+  
+}
+
+
+function render(){
+    document.getElementById('feed').innerHTML = getFeedHtml()    
+}
+
+render()
+
